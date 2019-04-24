235a5d9485ce51c4df71f4d9a09638d676b8010d
Chris Piccaro <chrispiccaro18@gmail.com>
2019-04-24 13:36:24 -0700

    User model tests passing

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,18 +1,17 @@
 require('../connect-db');
 const mongoose = require('mongoose');
 const User = require('../../lib/models/User');
+const { untokenize } = require('../../lib/utils/token');
 
 describe('User model tests', () => {
   it('has a username, hashed password, and profilePhotoUrl', () => {
     const user = new User({
       username: 'test',
-      passwordHash: 'randomstring'
     });
 
     expect(user.toJSON()).toEqual({
       _id: expect.any(mongoose.Types.ObjectId),
       username: 'test',
-      passwordHash: 'randomstring',
       profilePhotoUrl: 'https://i.pinimg.com/736x/9f/81/2d/9f812d4cf313e887ef99d8722229eee1--facebook-profile-profile-pictures.jpg'
     });
   });
@@ -34,18 +33,38 @@ describe('User model tests', () => {
     });
     expect(user._tempPassword).toEqual('test123');
   });
+
   it('creates hash with new user', async() => {
     const user = await User.create({
       username: 'test person',
       password: 'test123'
     });
-    console.log(typeof user._id, typeof user.passwordHash, typeof user.profilePhotoUrl);
-    expect(user).toEqual({
+
+    expect(user.toJSON()).toEqual({
       username: 'test person',
       _id: expect.any(mongoose.Types.ObjectId),
       profilePhotoUrl: expect.any(String),
-      passwordHash: expect.any(String),
-      __v: 0
+    });
+  });
+  it('can compare a good password', async() => {
+    const user = await User.create({
+      username: 'test person',
+      password: '123'
+    });
+    const result = await user.compare('123');
+    expect(result).toBeTruthy();
+
+  });
+  it('create a token', async() => {
+    const user = await User.create({
+      username: 'test person',
+      password: '123'
+    });
+    const token = user.authToken();
+    expect(untokenize(token)).toEqual({
+      _id: expect.any(String),
+      username: 'test person',
+      profilePhotoUrl: expect.any(String)
     });
   });
 });
