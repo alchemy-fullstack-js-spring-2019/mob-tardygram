61bc463eccd2b036d9076c4d057183cf4811de96
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-04-24 15:32:58 -0700

    data helpers

--- test/data-helpers.js
+++ test/data-helpers.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const connect = require('../lib/utils/connect');
+const seedData = require('./seed-data');
+const User = require('../lib/models/User');
+const Thread = require('../lib/models/Thread');
+const Comment = require('../lib/models/Comment');
 
 beforeAll(() => {
   return connect();
@@ -10,6 +14,25 @@ beforeEach(() => {
   return mongoose.connection.dropDatabase();
 });
 
+beforeEach(() => {
+  return seedData();
+});
+
 afterAll(() => {
   return mongoose.connection.close();
 });
+
+const prepare = model => JSON.parse(JSON.stringify(model));
+
+const createGetters = Model => {
+  return {
+    [`get${Model.modelName}`]: query => Model.findOne(query).then(prepare),
+    [`get${Model.modelName}s`]: query => Model.find(query).then(models => models.map(prepare))
+  };
+};
+
+module.exports = {
+  ...createGetters(User),
+  ...createGetters(Thread),
+  ...createGetters(Comment)
+};
