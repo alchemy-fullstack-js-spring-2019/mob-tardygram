4bdf7d5c83c11463b45afb7c6e30bec61d958717
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-04-24 14:08:30 -0700

    GET

--- test/routes/auth.test.js
+++ test/routes/auth.test.js
@@ -1,7 +1,5 @@
 require('dotenv').config();
-const mongoose = require('mongoose');
 const request = require('supertest');
-const connect = require('../../lib/utils/connect');
 const app = require('../../lib/app');
 const User = require('../../lib/models/User');
 require('../data-helpers');
@@ -27,7 +25,7 @@ describe('auth routes', () => {
       });
   });
 
-  it.only('can sign in a user', () => {
+  it('can sign in a user', () => {
     return User.create({
       username: 'Jeff',
       password: 'secret'
