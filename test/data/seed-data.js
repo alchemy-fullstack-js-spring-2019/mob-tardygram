078357e0274e9c1c2f04d2eb4fbf7277854d19ad
Parker Helland <hellandp@gmail.com>
2019-04-24 10:53:59 -0700

    write middleware and seeddata/data helpers

---
+++ test/data/seed-data.js
@@ -0,0 +1,17 @@
+const chance = require('chance').Chance();
+const User = require('../../lib/models/User');
+
+module.exports = ({
+  userCount = 10,
+  postCount = 25,
+  commentCount = 50,
+
+} = {}) => {
+  const users = [...Array(userCount)]
+    .map(() => ({
+      username: chance.name(),
+      passwordHash: chance.hash(20),
+      profilePhotoUrl: chance.url(),
+    }));
+  return User.create(users);
+};
