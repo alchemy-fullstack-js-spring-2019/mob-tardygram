447e36a8fb03ca01bb0300f2854f00af42a55d69
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-04-24 13:38:01 -0700

    auth signin test passing

---
+++ test/routes/auth.test.js
@@ -0,0 +1,54 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const request = require('supertest');
+const connect = require('../../lib/utils/connect');
+const app = require('../../lib/app');
+const User = require('../../lib/models/User');
+require('../data-helpers');
+
+describe('auth routes', () => {
+  it('signs up a new user', () => {
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        username: 'Jeff',
+        password: 'secret',
+        profilePhoto: 'pic.jpg'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          user: {
+            _id: expect.any(String),
+            username: 'Jeff',
+            profilePhoto: 'pic.jpg'
+          },
+          token: expect.any(String)
+        });
+      });
+  });
+
+  it.only('can sign in a user', () => {
+    return User.create({
+      username: 'Jeff',
+      password: 'secret'
+    })
+      .then(() => {
+        return request(app)
+          .post('/api/v1/auth/signin')
+          .send({
+            username: 'Jeff',
+            password: 'secret'
+          });
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          user: {
+            _id: expect.any(String),
+            username: 'Jeff'
+          },
+          token: expect.any(String)
+        });
+      });
+  });
+});
+
