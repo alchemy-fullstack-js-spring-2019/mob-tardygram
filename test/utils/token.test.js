21cadc8f373a3ffb88fc55004cb29aeb45712292
Chris Piccaro <chrispiccaro18@gmail.com>
2019-04-24 11:09:50 -0700

    token test passing

--- test/utils/token.test.js
+++ test/utils/token.test.js
@@ -2,8 +2,13 @@ const { tokenize, untokenize } = require('../../lib/utils/token');
 
 describe('token tests', () => {
   const payload = { username: 'joan', photoUrl: 'somelongstring' };
-  it('creates a token', async() => {
-    const token = await tokenize(payload);
+  it('creates a token', () => {
+    const token = tokenize(payload);
     expect(token).toEqual(expect.any(String));
+  });
+  it('takes a token and verifies that it is valid.  returns payload on valid toeken ', () => {
+    const token = tokenize(payload);
+    const body = untokenize(token);
+    expect(body).toEqual(payload);
   });
 });
