cf0217cf45b058154cbed39a3cdc5533d88bd23f
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-04-24 10:52:33 -0700

    can compare passwords/test passing

--- test/utils/hash.test.js
+++ test/utils/hash.test.js
@@ -1,4 +1,4 @@
-const { hash } = require('../../lib/utils/hash');
+const { hash, compare } = require('../../lib/utils/hash');
 
 describe('hashing functions', () => {
   it('hashes a password', () => {
@@ -6,6 +6,18 @@ describe('hashing functions', () => {
       .then(hashedPassword => {
         expect(hashedPassword).toEqual(expect.any(String));
         expect(hashedPassword).not.toEqual('password');
+      });
+  });
+
+  it('can compare passwords', () => {
+    const password = 'password';
+    
+    return hash(password)
+      .then(hashedPassword => {
+        return compare('password', hashedPassword);
+      })
+      .then(compareResult => {
+        expect(compareResult).toBeTruthy();
       });
   });
 });
