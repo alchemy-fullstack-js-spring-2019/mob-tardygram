27919217b4e061927614229ba38ebc1c5a85a231
Parker Helland <hellandp@gmail.com>
2019-04-24 16:46:09 -0700

    write model and pass tests

---
+++ test/models/Post.test.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Post = require('../../lib/models/Post');
+
+describe('post test', () => {
+  it('makes a model', () => {
+    const post = new Post({
+      user: new mongoose.Types.ObjectId,
+      photoUrl: 'photo',
+    });
+
+    expect(post.toJSON()).toEqual({
+      user: expect.any(mongoose.Types.ObjectId),
+      _id: expect.any(mongoose.Types.ObjectId),
+      photoUrl: 'photo',
+      tags: []
+    });
+  });
+
+  it('requires a user', () => {
+    const post = new Post({ });
+    const errors = post.validateSync().errors;
+    expect(errors.user.message).toEqual('Path `user` is required.');
+  });
+
+});
--- test/routes/auth.test.js
+++ test/routes/auth.test.js
@@ -69,5 +69,4 @@ describe('auth routes', () => {
       });
   });
 
-
 });
