ce86d697adface4799a6539112177136aea4b6a1
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-04-25 16:59:12 -0700

    delete comment working/test passing

--- test/routes/comments.test.js
+++ test/routes/comments.test.js
@@ -25,4 +25,31 @@ describe('comments routes', () => {
         });
       });
   });
+
+  it('can delete a comment by id', async() => {
+    const token = await getToken();
+    const user = await getUser();
+    const post = await getPost();
+    return request(app)
+      .post('/api/v1/comments')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        commentBy: user._id,
+        post: post._id,
+        comment: 'hola'
+      })
+      .then(createdComment => {
+        return request(app)
+          .delete(`/api/v1/comments/${createdComment.body._id}`)
+          .set('Authorization', `Bearer ${token}`);
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          commentBy: expect.any(String),
+          post: expect.any(String),
+          comment: 'hola',
+          _id: expect.any(String)
+        });
+      });
+  });
 });
