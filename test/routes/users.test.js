cab239eac11d5987b0e424917112bd4272e64343
Chris Piccaro <chrispiccaro18@gmail.com>
2019-04-26 14:10:40 -0700

    all user route agg tests passing

--- test/routes/users.test.js
+++ test/routes/users.test.js
@@ -8,4 +8,19 @@ describe('user route tests', () => {
       .get('/api/v1/users/popular');
     expect(res.body).toHaveLength(10);
   });
+  it('can return the top 10 users with the most posts', async() => {
+    const res = await request(app)
+      .get('/api/v1/users/prolific');
+    expect(res.body).toHaveLength(10);
+  });
+  it('can respond with the 10 users with the most comments', async() => {
+    const res = await request(app)
+      .get('/api/v1/users/leader');
+    expect(res.body).toHaveLength(10);
+  });
+  it('can respond with the 10 users with the most average comments per post', async() => {
+    const res = await request(app)
+      .get('/api/v1/users/impact');
+    expect(res.body).toHaveLength(10);
+  });
 });
