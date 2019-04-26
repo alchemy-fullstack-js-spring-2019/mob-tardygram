3ff83b6ed37b2f76179b847ba7a067b111cb8257
Parker Helland <hellandp@gmail.com>
2019-04-26 11:15:04 -0700

    seed data base

--- test/routes/aggregation.test.js
+++ test/routes/aggregation.test.js
@@ -3,4 +3,13 @@ const mongoose = require('mongoose');
 const request = require('supertest');
 const app = require('../../lib/app');
 const connect = require('../../lib/utils/connect');
-const 
+const { getUser, getPost, getComment, getComments } = require('../data/dataHelpers');
+
+describe('aggregation tests', () => {
+
+  it.only('gets the popular users', () =>{
+    getUser();
+    getPost();
+    getComment();
+  });
+});
