fb7586885c5b71eba3f41f0377303bc7932c3675
Parker Helland <hellandp@gmail.com>
2019-04-26 10:47:27 -0700

    finish seed data

--- test/data/seed-data.js
+++ test/data/seed-data.js
@@ -1,10 +1,12 @@
 const chance = require('chance').Chance();
 const User = require('../../lib/models/User');
+const Post = require('../../lib/models/Post');
+const Comment = require('../../lib/models/Comment');
 
 module.exports = ({
   userCount = 10,
-  // postCount = 25,
-  // commentCount = 50,
+  postCount = 25,
+  commentCount = 50,
 
 } = {}) => {
   const users = [...Array(userCount)]
@@ -13,5 +15,32 @@ module.exports = ({
       password: chance.hash(20),
       profilePhotoUrl: chance.url(),
     }));
-  return User.create(users);
+  return User.create(users)
+    .then(createdUsers => {
+      const posts = [...Array(postCount)]
+        .map(() => ({
+          user: chance.pickone(createdUsers)._id,
+          photoUrl: chance.url(),
+          caption: chance.sentence(),
+          tags: [chance.word()]
+        }));
+      return Promise.all([
+        Promise.resolve(createdUsers),
+        Post.create(posts)
+      ]);
+        
+    })
+    .then(([createdUsers, createdPosts]) => {
+      const comments = [...Array(commentCount)]
+        .map(() => ({
+          commentBy: chance.pickone(createdUsers)._id,
+          post: chance.pickone(createdPosts)._id,
+          comment: chance.sentence()
+        }));
+      return Promise.all([
+        Promise.resolve(createdUsers),
+        Promise.resolve(createdPosts),
+        Comment.create(comments)
+      ]);
+    }); 
 };
---
+++ test/routes/aggregation.test.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const request = require('supertest');
+const app = require('../../lib/app');
+const connect = require('../../lib/utils/connect');
+const 
