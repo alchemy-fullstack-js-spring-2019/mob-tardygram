a4bc1ae2d88ce60349de698d620a919bc82714da
Parker Helland <hellandp@gmail.com>
2019-04-26 11:41:31 -0700

    added compass aggregation

--- test/data/makeData.js
+++ test/data/makeData.js
@@ -1,3 +1,7 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 require('../../lib/utils/connect')();
-require('./seed-data')();
+require('./seed-data')()
+  .then(() => {
+    return mongoose.connection.close();
+  });
